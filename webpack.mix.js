5293af45cad2174989f9fef6f8735aee870b93ae
Daniel Keeble <danielkeeble94@googlemail.com>
2020-05-05 14:33:38 +0100

    cf

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,12 @@ const mix = require('laravel-mix');
 
 require('laravel-mix-tailwind');
 
-mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
-   .tailwind('./tailwind.config.js');
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css')
+  .tailwind('./tailwind.config.js');
 
 if (mix.inProduction()) {
   mix
-   .version();
+    .version();
 }
